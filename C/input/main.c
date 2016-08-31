cbad36a326dd4370ad4c7598bffda48a2a30ed25
Jeff Anderson <jeff.anderson@digecor.com>
2016-08-30 19:32:09 -0600

    Ask for age

--- C/input/main.c
+++ C/input/main.c
@@ -2,17 +2,27 @@
 
 // Demonstrates basic command line input.
 int main(void) {
-	char name[40];
+	char name[40] = "Nameless Wanderer";
+	int age = 0;
 	// Prompt for input.  No trailing newline.
 	printf("Enter your name: ");
 	
 	// 39 so there is 1 char left for trailing null char.
 	// Reads a formatted input string from stdin into 1 or more variables.
 	// Note that space delimits the format fields, so typing 'Jeff Anderson' will get you just
-	// 'Jeff'.	
+	// 'Jeff'.
+	//
+	// WARNING: scanf does not do bounds checking.  If you use a formatter that reads in more
+	// characters than your array can hold, you'll likely get a seg fault.
+	// If user just enters too many chars, these will still be in the buffer when the next scanf is
+	// called.  So, you probably need to flush this between scanf calls.
 	scanf("%39s", name);
 
-	printf("Hello, %s!\n", name);
+	printf("How old are you? ");
+	scanf("%i", &age); // scanf wants a pointer, address, or array
+
+
+	printf("Hello, %s (age %i)!\n", name, age);
 
 } // main()
 
