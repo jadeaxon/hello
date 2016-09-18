fc6813e7442321e9a704768b95d97060aec621fd
Jeff Anderson <jeff.anderson@digecor.com>
2016-09-17 21:55:03 -0600

    Add default.sh
    If present, default.sh should be run when you run just '.' as a command.
    
    The . file.ext means "do the default action to file.ext given its type".
    It's kind of like double-clicking on a file via Desktop interface.  I
    interpret lone . to mean "do the default action for this directory".
    Now, the default direction of '. <dir>' is to cd to <dir>.  But moving
    to the same dir is pointless, so we can do something more elaborate like
    run default.sh (if present).
    
    Also, I'm thinking of having a default default action for directories
    that don't have a default.sh.

---
+++ C/hello/default.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Otherwise pipelines succeed if just their last command exits 0.
+set -o pipefail 
+
+S=$(basename $0)
+
+if gcc main.c |& tee make.out; then
+	rm make.out
+	if [ -x a.out ]; then # Linux
+		./a.out
+	elif [ -x a.exe ]; then # Cygwin 
+		./a.exe
+	else # No executable found.
+		echo "$S: ERROR: No executable found!"
+		exit 1
+	fi
+else # Compilation failed.
+	# Edit errors as a Vim quicklist.
+	vim -q make.out
+fi
+
+
