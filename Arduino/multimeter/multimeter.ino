794dc33b389d035a7301ea6ccc1e5be76a61fefd
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-22 15:35:01 -0700

    Make simple sketch to test pin with multimeter

---
+++ Arduino/multimeter/multimeter.ino
@@ -0,0 +1,29 @@
+// The point of this program is to let you switch a pin on and off so you can measure its voltage
+// with a multimeter.
+
+const int PIN = 3;
+
+// Sets up output pin and serial comms.
+void setup() {
+	Serial.begin(9600);
+	pinMode(PIN, OUTPUT);
+
+}
+
+// Switches a pin high or low depending on input from user via serial port.
+void loop() {
+	if ( Serial.available() ) {
+		char c = Serial.read();
+		if (c == '0') {
+			digitalWrite(PIN, LOW);
+		}
+		else if (c == '1') {
+			digitalWrite(PIN, HIGH);
+		}
+		else {
+			Serial.println("ERROR: Invalid input.");
+		}
+	}
+}
+
+
