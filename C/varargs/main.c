e0724b8a5a95aebf4f0a40d6508e59e60c63bc48
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-23 15:58:34 -0700

    Answer questions

--- C/varargs/main.c
+++ C/varargs/main.c
@@ -3,10 +3,15 @@
 #include <stdarg.h> // For variadic functions.
 #include <stdio.h> // printf().
 
+// First arg args tells us how many more args to process.
 int sum(int args, ...) {
 	va_list ap; // The vararg list.  
 	// Q. Why is the va_list called ap?
+	// A. Probably means 'arg pointer'.
+	//
 	// Q. Does the first arg have to tell you how many more args there are?	
+	// A. It has to know somehow.  Whether by explicit count, implicit count
+	// (like in printf field specifiers) or a sentinel value.
 	va_start(ap, args); // The variable list starts after the args argument.
 	int sum = 0;
 	for (int i = 0; i < args; i++) {
