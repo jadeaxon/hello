0285a49130d5de5ee1c07ac73f45491fa43392cf
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-26 08:08:42 -0700

    Use line buffer to parse content length to prevent hang on last recv()
    Yeah, converting the raw buffer reads into a sequence of lines and then
    parsing the content length out of the HTTP header is necessary otherwise
    you hang indefinitely on the last blocking recv() call.
    
    Of course, the example from the book did not even try to deal with these
    unfortunate realities.

--- C/sockets/tcpip_socket/web_client.c
+++ C/sockets/tcpip_socket/web_client.c
@@ -9,7 +9,7 @@
 #include <signal.h> // SIGINT
 
 // Nice Things
-// It currently is deployed to $HOME/lib/.
+// It currently is deployed to $HOME/lib/ and /usr/local/lib.
 #include <nt_error.h> // nt_error()
 #include <nt_signal.h> // nt_register_signal_handler()
 
@@ -23,6 +23,7 @@ int say(int socket, char* s);
 
 // Requests a hard-coded web page via HTTP.  Prints response to stdout.
 int main(int argc, char** argv) {
+	const int D = 0; // Print debug output?	
 	// Connect via IP address.
 	/*
 	int s = socket(PF_INET, SOCK_STREAM, 0);
@@ -51,17 +52,88 @@ int main(int argc, char** argv) {
 	say(socket, "Host: nethackwiki.com\r\n\r\n");
 	
 	// Read the response.
+	int total_bytes = 0;
 	int bytes = recv(socket, buf, 255, 0);
+	int expected_bytes = -1;
+	char line[1024]; // Buffer for HTTP header lines.	
+	char* li = line; // Line buffer index.	
+	int beyond_headers = 0; // Are we done processing HTTP headers?
+	
 	while (bytes) {
 		e(bytes, "Can't read from server");
 		buf[bytes] = '\0';
-		// TO DO: Check 'Content Length:' HTTP header so we don't hang forever on recv().
 		printf("%s", buf);
+		D && printf("___\n"); // Show receive buffer boundaries.
+	
+		// We have to turn the raw response chunks into individual lines so we can parse out the
+		// response length so that we don't hang forever on the last recv().  A line may be broken
+		// between two buffer loads of response and a buffer load may contain multiple lines.
+		// Also, the content length is *after* all headers have been processed.  It does not count
+		// the header bytes.	
+		char* i = buf;
+		int buffer_index = 0;
+		int lines = 0;
+		while (1) {
+			int line_complete = 0;
+			while (*i != '\0') {
+				D && printf("buf[%d] = %c\n", buffer_index, *i);
+				if (*i == '\n') {
+					line_complete = 1;
+					li--; // Headers use \r\n, so we'll chop that off.
+					i++; buffer_index++;
+					break;
+				}
+				*li = *i; // Fill the line buffer.
+				li++;
+				i++; buffer_index++;
+			} // next char in response buffer
+			if (line_complete) {
+				*li = '\0';	
+				D && printf("LINE: %s\n", line);
+				
+				// A single empty line designates the end of HTTP headers.		
+				if (!beyond_headers && strlen(line) == 0) {
+					D && printf("\n");
+					D && printf("Finished processing HTTP headers.\n");	
+					D && printf(" expected_bytes = %d\n", expected_bytes);
+					beyond_headers = 1;
+					// Part of this buffer load is response data.
+					D && printf(" bytes = %d\n", bytes);
+					D && printf(" buffer_index = %d\n", buffer_index);
+					total_bytes += (bytes - buffer_index); // ??? Feels like this is off by one, but total comes out right.
+					D && printf(" total_bytes = %d\n", total_bytes);
+				}
+
+				// Check 'Content Length:' HTTP header so we don't hang forever on recv().
+				if ( strstr(line, "Content-Length: ") == line ) {
+					nt_keep_chars(line, "0123456789");
+					expected_bytes = atoi(line);
+					D && printf("expected_bytes = %d\n", expected_bytes);
+				}
+				li = line; // Point back to beginning of line.
+				lines++;
+			}
+			else { // Line was not complete.
+				break; // Need to read more response from server.
+			}
+			/*
+			if (lines >= 2) {
+				exit(1);
+			}
+			*/
+		} // next potential line in response buffer
+
+		// Don't read anymore bytes if we've already read the expected amount.
+		if ((expected_bytes != -1) && (total_bytes >= expected_bytes)) {
+			break; // We've read all the bytes in the response.
+		}
+		
 		// It seems if server does not close connection after all data sent, this call will hang forever.
 		bytes = recv(socket, buf, 255, 0);
+		if (beyond_headers) total_bytes += bytes;
+		D && printf("total_bytes = %d\n", total_bytes);
 	} // next subseq of bytes
 
-	puts("HERE!");
 	close(socket);
 
 } // main(void)
