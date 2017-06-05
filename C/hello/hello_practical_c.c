4577f683738503e2a3b9ab8ab0e6d05b26c70cc7
Jeff Anderson <jeff.anderson@digecor.com>
2017-06-05 12:00:34 -0600

    Comment the code a bit

--- C/hello/hello_practical_c.c
+++ C/hello/hello_practical_c.c
@@ -1,7 +1,15 @@
-#include <stdio.h> 
+// The C Standard Library I/O routines.
+#include <stdio.h> // For printf().
 
+
+//=============================================================================
+// Main
+//=============================================================================
+
+// Prints a message to the console.
 int main() { 
 	printf("Hello again, C!\n"); 
 	return (0); 
 }
+
 
