f60c7257bd9a2fb2a98971faad7bbdb4e94d65cc
Jeff Anderson <jeff.anderson@digecor.com>
2017-09-30 15:47:21 -0600

    Add a new line and make only it print

--- Python 3/lp3thw/exercise1/ex1.py
+++ Python 3/lp3thw/exercise1/ex1.py
@@ -1,8 +1,10 @@
-print("Hello, world!")
-print("Hello again.")
-print("I like typing this.")
-print("This is fun.")
-print("Yay!  Printing.")
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
+# print("Hello, world!")
+# print("Hello again.")
+# print("I like typing this.")
+# print("This is fun.")
+# print("Yay!  Printing.")
+# print("I'd much rather you 'not'.")
+# print('I "said" do not touch this.')
+print("This is another line.")
+
 
