5d4521ce10f20665adf361bdf9b0a0fbed5ddc10
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-01 04:46:52 -0600

    Use floating point
    :%s/\v([0-9])+/\1.0/gc

---
+++ Python 3/lp3thw/exercise3/ex3_4.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+# Print a message.
+print("I will now count my chickens:")
+
+# Calculate number of hens.
+print("Hens", 25.0 + 30.0 / 6.0)
+# Calculate number of roosters.
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# Print a message.
+print("Now I will count the eggs:")
+
+# Do a bunch of highly-unparenthesized math.
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Speculate on what an expression will evaluate to.
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+# Test hypothesis.
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Print expression evaluation.
+print("What is 3.0 + 2.0?", 3.0 + 2.0)
+# Print expression evaluation.
+print("What is 5.0 - 7.0?", 5.0 - 7.0)
+
+# Talk to myself.
+print("Oh, that's why it's False.")
+
+# Ask a question without using a question mark.
+print("How about some more.")
+
+# Use a binary relational operator.
+print("Is it greater?", 5.0 > -2.0)
+# Use a binary relational operator.
+print("Is it greater or equal?", 5.0 >= -2.0)
+# Use a binary relational operator.
+print("Is it less or equal?", 5.0 <= -2.0)
+
