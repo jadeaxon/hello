57f6449af414dbf46373cee7cf90df6e01cd826a
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-01 12:03:23 -0600

    Do drill 3

--- Python 3/lp3thw/exercise4/ex4.py
+++ Python 3/lp3thw/exercise4/ex4.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
+# Total number of cars.
 cars = 100
+# Number of people, including driver, each car can hold.
 space_in_a_car = 4.0
+# Number of drivers.
 drivers = 30
+# Number of passengers.
 passengers = 90
+# Cars not driven due to lack of drivers.
+# Where's my self-driving car Google, Apple, etc.?
 cars_not_driven = cars - drivers
+# Number of cars actually driven.
 cars_driven = drivers
+# Total person transportation capacity of the carpool.
 carpool_capacity = cars_driven * space_in_a_car
+# Average number of passengers in each car.
 average_passengers_per_car = passengers / cars_driven
 
 print("There are", cars, "cars available.")
