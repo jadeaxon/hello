b23dfbca7dd0f4255725886376b412720ba28632
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-01 04:36:07 -0600

    Add a comment before each line

--- Python 3/lp3thw/exercise3/ex3.py
+++ Python 3/lp3thw/exercise3/ex3.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
 
+# Print a message.
 print("I will now count my chickens:")
 
+# Calculate number of hens.
 print("Hens", 25 + 30 / 6)
+# Calculate number of roosters.
 print("Roosters", 100 - 25 * 3 % 4)
 
+# Print a message.
 print("Now I will count the eggs:")
 
+# Do a bunch of highly-unparenthesized math.
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# Speculate on what an expression will evaluate to.
 print("Is it true that 3 + 2 < 5 - 7?")
+# Test hypothesis.
 print(3 + 2 < 5 - 7)
 
+# Print expression evaluation.
 print("What is 3 + 2?", 3 + 2)
+# Print expression evaluation.
 print("What is 5 - 7?", 5 - 7)
 
+# Talk to myself.
 print("Oh, that's why it's False.")
 
+# Ask a question without using a question mark.
 print("How about some more.")
 
+# Use a binary relational operator.
 print("Is it greater?", 5 > -2)
+# Use a binary relational operator.
 print("Is it greater or equal?", 5 >= -2)
+# Use a binary relational operator.
 print("Is it less or equal?", 5 <= -2)
 
