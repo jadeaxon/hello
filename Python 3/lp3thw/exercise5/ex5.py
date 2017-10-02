cc5036fae40ec7816c2c4808cb43f9eb50c3cd4d
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-01 18:26:08 -0600

    Do lp3thw ex5

---
+++ Python 3/lp3thw/exercise5/ex5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+my_name = 'Jeff Anderson'
+my_age = 41 # The sad truth.
+my_height = 72 # inches
+my_weight = 195.4 # lbs
+my_eyes = 'hazel'
+my_teeth = 'white enough'
+my_hair = 'gone'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He weighs {my_weight} pounds.")
+print(f"That's technically overweight for his height.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the toothpaste.")
+
+# This line is tricky, try to get it exactly right.
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight}, then I get {total}.")
+
