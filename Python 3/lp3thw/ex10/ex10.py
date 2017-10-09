c71e18f8f4e7e2c357a7a23a7237e03023c557b0
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-09 17:54:13 -0600

    Do lp3thw ex10

---
+++ Python 3/lp3thw/ex10/ex10.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Assign a string containing at least one escaped char.
+tabby_cat = "\tI'm tabbed in."
+# Assign a string containing at least one escaped char.
+# Note that no escape is necessary for #.
+persian_cat = "# I'm split\non a line."
+# Assign a string containing at least one escaped char.
+backslash_cat = "I'm \\ a \\ cat."
+
+# Assign a triple-quote string which includes escaped chars.
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# Print.
+print(tabby_cat)
+# Print.
+print(persian_cat)
+# Print.
+print(backslash_cat)
+# Print.
+print(fat_cat)
+
