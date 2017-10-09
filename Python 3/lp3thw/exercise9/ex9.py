13688d47bb8828f4ba4635c7c9f4c253239ef235
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-09 15:51:43 -0600

    Do lp3thw ex9

---
+++ Python 3/lp3thw/exercise9/ex9.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Here's some new strange stuff, remember to type it exactly.
+# Space-separated values.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Newline-separated values.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print some values.
+print("Here are the days:", days)
+# Print some values.
+print("Here are the months:", months)
+
+# Use a triple quote so string can span multiple lines.
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+
+""")
+
