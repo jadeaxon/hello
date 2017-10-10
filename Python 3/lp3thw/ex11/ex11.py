4f84ff00a1209cc6490af326552946d9c7955e79
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-10 14:05:24 -0600

    Do lp3thw ex11

---
+++ Python 3/lp3thw/ex11/ex11.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Prompt user.
+print("How old are you?", end=' ')
+# Get user input.
+age = input()
+# Prompt user.
+print("How tall are you?", end=' ')
+# Get user input.
+height = input()
+# Prompt user.
+print("How much do you weight?", end=' ')
+# Get user input.
+weight = input()
+# Expand vars.
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
