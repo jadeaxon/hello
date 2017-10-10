96cfbecd4eb80be83d4c2df4d6b66fcee8dc237f
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-10 14:18:49 -0600

    Do the ex11 drills

---
+++ Python 3/lp3thw/ex11/ex11_drills.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import readline
+
+show = input("What's your favorite TV show? ")
+movie = input("What's your favorite movie? ")
+book =  input("What's your favorite book? ")
+
+print(f"""
+Your favorite book is {book}.
+Your favorite movie is {movie}.
+Your favorite TV show is {show}.
+""")
+
+
+
