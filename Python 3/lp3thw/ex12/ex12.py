f54773dd760bc1f8a56919b3304c2b63b09015bb
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-10 19:19:50 -0600

    Do lp3thw ex12

---
+++ Python 3/lp3thw/ex12/ex12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Combine the prompt with the call to input().
+age = input("How old are you? ")
+# Combine the prompt with the call to input().
+height = input("How tall are you? ")
+# Combine the prompt with the call to input().
+weight = input("How much do you weigh? ")
+# Print formatted string with user-input vars.
+print(f"So, you're {age} old, {height} tall, and {weight} heavy.")
+
