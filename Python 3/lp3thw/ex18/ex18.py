90468cbe1fd3d95d13cafb6a569077374521bc58
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-12 05:44:29 -0600

    Do lp3thw ex18
    We've now defined our own functions.

---
+++ Python 3/lp3thw/ex18/ex18.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# This one is like your scripts with argv.
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# Ok, that *args is actually pointless.  We can just do this:
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# This just takes one arg.
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+# This one takes no args.  Niladic.
+def print_none():
+    print("I got nothin'.")
+
+# Now use these functions.
+
+print_two("Zed", "Shaw")
+print_two_again(arg1="SHaw", arg2="Zed")
+print_two_again("Zed", "Shaw")
+print_one("First!")
+print_none()
+
