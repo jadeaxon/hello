b26af58af9d1e8c4d0a4d8fd9f958f6e12e9c812
Jeff Anderson <jeff.anderson@digecor.com>
2017-10-14 10:34:21 -0600

    Do lp3thw ex34
    Random access to list elements.  0-based indexing.

---
+++ Python 3/lp3thw/ex34/ex34.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# The point that list indexes are 0-based.
+
+l = ['apple', 'orange', 'banana', 'kiwi', 'pineapple', 'pear']
+print(l[0]) # First.
+print(l[-1])
+print(l[7]) # IndexError
+
+
