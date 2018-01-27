a4684a519d714c85cdfb681027a8a8bb7db1b2e3
Jeff Anderson <jeff.anderson@digecor.com>
2018-01-27 16:49:14 -0700

    Recognize curl

--- JavaScript/Node/server2.js
+++ JavaScript/Node/server2.js
@@ -3,13 +3,26 @@
 // Import the core HTTP networking module.
 var http = require("http");
 
+var log = console.log;
+
 // Handles an HTTP request.
 function requestHandler(request, response) {
-	console.log("Request received.");
-	console.log("Request headers:");
-	console.log(request.headers);
+	var client = "HTTP client";
 
-	response.write("Hello, HTTP client!");
+	log("Request received.");
+	log("Request method: " + request.method); // The HTTP method.
+	log("Request URL: " + request.url);
+	log("Request headers:");
+	log(request.headers); // request.headers is a map object.
+	var userAgent = request.headers["user-agent"];
+	log("user-agent: " + userAgent);
+	if (userAgent.indexOf("curl") !== -1) {
+		client = "curl";
+	}
+
+	// Once you do the first write() you can no longer change response headers.
+	response.setHeader("content-type", "text/plain"); // MIME type.
+	response.write("Hello, " + client + "!");
 	response.end();
 }
 
@@ -19,6 +32,6 @@ var server = http.createServer(requestHandler);
 // Listen for requests on TCP port 3000.
 var port = 3000;
 server.listen(port);
-console.log("Server listening for requests at http://localhost:" + port + ".");
+log("Server listening for requests at http://localhost:" + port + ".");
 
 
