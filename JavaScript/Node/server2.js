2fdbda40febd92bbe8c1257a8d9fdc9bf6593485
Jeff Anderson <jeff.anderson@digecor.com>
2018-01-27 15:51:50 -0700

    Log the request headers

--- JavaScript/Node/server2.js
+++ JavaScript/Node/server2.js
@@ -3,16 +3,18 @@
 // Import the core HTTP networking module.
 var http = require("http");
 
-// Create an HTTP server.
-var server = http.createServer(
-	// This is the request handler function.
-	function(request, response) {
-		console.log("Request received.");
+// Handles an HTTP request.
+function requestHandler(request, response) {
+	console.log("Request received.");
+	console.log("Request headers:");
+	console.log(request.headers);
 
-		response.write("Hello, HTTP client!");
-		response.end();
-	}
-);
+	response.write("Hello, HTTP client!");
+	response.end();
+}
+
+// Create an HTTP server.
+var server = http.createServer(requestHandler);
 
 // Listen for requests on TCP port 3000.
 var port = 3000;
