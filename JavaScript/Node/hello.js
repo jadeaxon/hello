a48a26acbbbd316e45694028de62cc680e103f66
Jeff Anderson <jeff.anderson@digecor.com>
2018-01-27 12:27:39 -0700

    Use inheritance to implement the AdvancedGreeter subclass

--- JavaScript/Node/hello.js
+++ JavaScript/Node/hello.js
@@ -3,12 +3,17 @@
 // To run: node hello.js (just ./hello.js with the shebang).
 console.log("Hello, Node.js!");
 
-// Use a local package.
-var mymodule = require("./mymodule");
+// Use a local package module.
+// Loads the main module from "mypackage" package.
+var mymodule = require("./mypackage");
 mymodule.helloWorld();
 
 // Use a class from the local packages.
 var greeter = new mymodule.Greeter();
 greeter.greet();
+
+var AdvancedGreeter = require("./mypackage").AdvancedGreeter;
+var advancedGreeter = new AdvancedGreeter("inheritance!");
+advancedGreeter.greet();
 
 
--- JavaScript/Node/mypackage/mymodule.js
+++ JavaScript/Node/mypackage/mymodule.js
@@ -1,16 +1,52 @@
+// This is the main module within the mypackage package.
+// You set which module is the main module in package.json.
+// By default, the main module is index.js.
+// package.json is the metadata file for the package.
+
+// Each module implicitly has a module.exports.
+// This is implicitly aliased to exports.
+// When you call require() and it finds your module,
+// whatever is assigned to module.exports gets returned.
+
+// A normal module function.
 var helloWorld = exports.helloWorld = function() {
 	console.log("Hello, world!");
-}
+};
 
+// A constructor function.  More or less represents a class.
 // Contructs a Greeter object.  Call as follows:
 // var greeter = new Greeter();
 var Greeter = exports.Greeter = function() {
 	this.greeting = "Hello, OO JavaScript!";
+};
+
+// A method of the Greeter class.
+// Greets the caller.
+Greeter.prototype.greet = exports.Greeter.prototype.greet = function () {
+	console.log(this.greeting);
+};
+
+// Constructs an AdvancedGreeter object.
+// This is a subclass of Greeter.
+var AdvancedGreeter = exports.AdvancedGreeter = function(greetee) {
+	Greeter.call(this);
+	this.greeting = "Hello, " + greetee;
+	this.greetings = 0;
+};
+
+// Override the greet() method, but call it in the override.
+AdvancedGreeter.prototype.greet = exports.AdvancedGreeter.prototype.greet = function() {
+	exports.Greeter.prototype.greet.call(this); // Call the base class method.
+	this.greetings++;
 }
 
-// Greets the caller.
-exports.Greeter.prototype.greet = function () {
-	console.log(this.greeting);
-}
+// Create the inheritance relationship.
+var inherits = require("util").inherits;
+inherits(AdvancedGreeter, Greeter);
+
+
+
+
+	
 
 
