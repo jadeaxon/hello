11d3b45d069019d7db634141ce645b8bafcfb754
Jeff Anderson <jeff.anderson@digecor.com>
2018-01-27 15:41:24 -0700

    Log server start

--- JavaScript/Node/server2.js
+++ JavaScript/Node/server2.js
@@ -15,6 +15,8 @@ var server = http.createServer(
 );
 
 // Listen for requests on TCP port 3000.
-server.listen(3000);
+var port = 3000;
+server.listen(port);
+console.log("Server listening for requests at http://localhost:" + port + ".");
 
 
