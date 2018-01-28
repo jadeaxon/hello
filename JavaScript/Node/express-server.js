9408f741539703859991682f613b2dd5884b235a
Jeff Anderson <jeff.anderson@digecor.com>
2018-01-27 19:57:10 -0700

    Let the web server show directory contents
    If an index.html is present, this will override the directory listing.

--- JavaScript/Node/express-server.js
+++ JavaScript/Node/express-server.js
@@ -4,10 +4,13 @@
 
 var log = console.log;
 var express = require('express');
+var serveIndex = require('serve-index');
 var app = express();
 // __dirname is the current working dir of this process.
 // serveStatic is a Connect/Express middleware for serving static web pages.
-app.use(express.static(__dirname + '/html'))
+var docroot = __dirname + '/html';
+app.use(express.static(docroot));
+app.use(serveIndex(docroot)); // Display list of files in a directory.
 app.listen(3000);
 log("Listening for HTTP requests on port 3000.");
 
