cd5887494b6dfd787d21c6d546fe251b0650c588
Jeff Anderson <jeff.anderson@digecor.com>
2018-01-27 17:41:29 -0700

    Mod server to serve up anything in html subdir
    I use the mime module to look up MIME types.
    This introduces a dep on a non-core module.
    So, I had to turn this dir into a package.
    npm init
    npm install mime
    Thus, we now have package.json and package-lock.json.
    The index.html page now uses a bit of JavaScript.
    No need to check in node_modules/ since package.json tracks deps.
    You can just use "npm install" to download all the deps again.

---
+++ JavaScript/Node/html/main.js
@@ -0,0 +1,5 @@
+// Add to <body> when browser window loads.
+window.onload = function () {
+	document.body.innerHTML += "<strong>JavaScript!</strong>";
+}
+
--- JavaScript/Node/server3.js
+++ JavaScript/Node/server3.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
+// This web server can serve up anything in the html/ subdir
+// of the directory where it is launched.
+
 var http = require("http");
 var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+
 var log = console.log;
 
 // Generates an HTTP 404 response (resource not found).
@@ -14,18 +20,40 @@ function send404(response) {
 // Handles incoming HTTP GET requests.
 function requestHandler(request, response) {
 	if (request.method === "GET") {
-		if (request.url === "/") {
-			response.writeHead(
-				200,
-				{'content-type': 'text/html'}
-			);
-			fs.createReadStream("html/index.html").pipe(response);
-		}
-		else { // index.html DNE
+		var url = request.url; // Local URL.
+		if (url === "/") url = "/index.html";
+		var filepath = path.resolve("./html" + url);
+		var ext = path.extname(filepath);
+
+		var mimeType = mime.getType(ext);
+		if (!mimeType) {
 			send404(response);
+			return;
 		}
+
+		// Does this run async?
+		fs.exists(
+			filepath,
+			// What to do if the given path exists or not.
+			function (exists) {
+				if (!exists) {
+					// Hmmm, this does know about response.
+					// So, it is an anonymous closure.
+					send404(response);
+					return;
+				}
+				response.writeHead(
+					200,
+					{'content-type': mimeType}
+				);
+				fs.createReadStream(filepath).pipe(response);
+			} // function (exists)
+		); // fs.exists
 	}
-}
+	else { // Not a GET request.
+		send404(response);
+	}
+} // requestHandler()
 
 var server = http.createServer(requestHandler);
 var port = 3000;
