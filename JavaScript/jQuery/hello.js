941fcb14f22c46205757b9340bd5aa30afe473fb
Jeff Anderson <jeff.anderson@digecor.com>
2018-01-28 13:49:33 -0700

    Create a simple web page that uses jQuery

---
+++ JavaScript/jQuery/hello.js
@@ -0,0 +1,14 @@
+// jQuery gets imported by the HTML file.
+// You can do it from a JavaScript file, but it isn't as concise.
+
+// Register a click event handler for a button.
+// When clicked, the CSS class "big" is added to the paragraph.
+$("button#enlarge").on("click", function() {
+	$("p#alice").addClass("big");
+});
+
+// Register a click event handler for a button.
+// When clicked, the CSS class "big" is removed from the paragraph.
+$("button#reduce").on("click", function() {
+	$("p#alice").removeClass("big");
+});
