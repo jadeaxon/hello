0b4e102ef029019dba300d5bd692a6c651968105
Jeff Anderson <jeff.anderson@digecor.com>
2018-02-03 01:48:17 -0700

    Fix if statement to actually work

--- PL-SQL/if.sql
+++ PL-SQL/if.sql
@@ -1,14 +1,16 @@
-SET SERVEROUTPUT ON SIZE 1000000
+-- This only has to be set once per session.
+SET SERVEROUTPUT ON SIZE 1000000;
 
+-- Here we have the declaration section of an anonymous block.
 DECLARE
 	truth BOOLEAN := TRUE;
 	lies BOOLEAN := FALSE;
 BEGIN
 	IF lies THEN
-		dbms_ouput.put_line('Dirty lies.');
-	ELSIF NOT truth THEN
+		dbms_output.put_line('Dirty lies.');
+	ELSIF (NOT truth) THEN
 		dbms_output.put_line('There is no truth.');
-	ELSE truth OR lies
+	ELSE
 		-- Note that we use '' for a literal ' here.
 		dbms_output.put_line('It''s complicated.');
 	END IF;
