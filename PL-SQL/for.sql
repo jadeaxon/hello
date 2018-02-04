b8e6cae89fdd5ce9b3e00d1e3ec589201a846e38
Jeff Anderson <jeff.anderson@digecor.com>
2018-02-03 20:28:17 -0700

    Loop using a cursor

--- PL-SQL/for.sql
+++ PL-SQL/for.sql
@@ -1,12 +1,21 @@
 SET SERVEROUTPUT ON SIZE 1000000
 
--- Loop over a range of numbers.
--- Ranges in PL/SQL are inclusive (unlike in Python).
 BEGIN
+	-- Loop over a range of numbers.
+	-- Ranges in PL/SQL are inclusive (unlike in Python).
 	FOR i IN 1 .. 10 LOOP
 		dbms_output.put_line(i);
 	END LOOP;
+	dbms_output.put(chr(10));
+
+	-- Loop using a cursor.
+	-- Dual is a weird pseudo table that has one column named dummy.
+	-- It has one row containing the value 'X'.
+	FOR c IN (select * from dual) LOOP
+		dbms_output.put_line(c.dummy);
+	END LOOP;
 END;
 /
+
 
 
