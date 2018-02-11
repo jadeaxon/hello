92003a6f3aac5b9af7b6bd0146be7b9bdefce234
Jeff Anderson <jeff.anderson@digecor.com>
2018-02-11 08:36:15 -0700

    Add toggle button for an image using jQuery

--- JavaScript/jQuery/hello.js
+++ JavaScript/jQuery/hello.js
@@ -3,12 +3,20 @@
 
 // Register a click event handler for a button.
 // When clicked, the CSS class "big" is added to the paragraph.
-$("button#enlarge").on("click", function() {
+$("button#enlarge").on("click", function () {
 	$("p#alice").addClass("big");
 });
 
 // Register a click event handler for a button.
 // When clicked, the CSS class "big" is removed from the paragraph.
-$("button#reduce").on("click", function() {
+$("button#reduce").on("click", function () {
 	$("p#alice").removeClass("big");
 });
+
+// Toggle visibility of the logo image when this button is pressed.
+$("button#toggle").on("click", function () {
+	$("img#logo").toggle("slow");
+});
+
+
+
