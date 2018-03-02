1b5f3e50d151a970a54ad714a5f3f53a09f126d4
Jeff Anderson <jeff.anderson@digecor.com>
2018-03-02 10:27:07 -0700

    Simplify

--- JavaScript/AngularJS/Material/hello/app/controller.js
+++ JavaScript/AngularJS/Material/hello/app/controller.js
@@ -1,5 +1,8 @@
 // theApp is a global var we created in main.js.
 // Note sure what this function-def-inside () syntax is about.
+// All it does is invoke the function in its own scope.
+
+/*
 (function(app){
 	'use strict';
 
@@ -9,4 +12,14 @@
 	});
 
 })(theApp);
+*/
+
+// Wrapping the code in the function again seems like needless complexity.
+'use strict';
+theApp.controller("appController", function($scope) {
+	$scope.title = "Welcome to Angular Material";
+	// We can shove this in the HTML template using {{<expression>}} syntax.
+	$scope.message = "This message will appear under the title.";
+});
+
 
--- JavaScript/AngularJS/Material/hello/app/main.js
+++ JavaScript/AngularJS/Material/hello/app/main.js
@@ -1,7 +1,13 @@
-// WTF.
-var theApp = (function(angularRef) {
+// All this accomplishes is calling the function in its own scope.
+/*
+var theApp = (function (angularRef) {
 	'use strict';
 	return angularRef.module('helloAngularMaterialApp', ['ngMaterial']);
 })(angular);
+*/
+
+// Yes, it looks like it was just needless complexity.
+'use strict';
+var theApp = angular.module('helloAngularMaterialApp', ['ngMaterial']);
 
 
