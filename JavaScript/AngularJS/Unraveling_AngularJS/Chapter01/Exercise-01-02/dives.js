ff56eb3be73d6b95ccf99e5ff525bee38e0a3ca4
Jeff Anderson <jeff.anderson@digecor.com>
2018-07-08 14:57:57 -0600

    Comment the JavaScript file that has the dives data

--- JavaScript/AngularJS/Unraveling_AngularJS/Chapter01/Exercise-01-02/dives.js
+++ JavaScript/AngularJS/Unraveling_AngularJS/Chapter01/Exercise-01-02/dives.js
@@ -1,3 +1,6 @@
+// Instead of baking the data into the HTML file, we've factored it out into a JavaScript object.
+// Could also have been loaded from a JSON file hosted on a remote server.
+// Or maybe it's in a database table and a service queries that and translates it into JSON.
 var dives = [
   {
     site: 'Abu Gotta Ramada',
@@ -16,4 +19,7 @@ var dives = [
     location: 'Budapest, Hungary',
     depth: 98,
     time: 62
-  }];
+  }
+];
+
+
