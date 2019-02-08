a63858aec0c96f8b31a40958ab9d672b9dcd395b
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-07 19:14:25 -0700

    Comment the files

--- JavaScript/AngularJS/lajs/ch02/public_html/js/app.js
+++ JavaScript/AngularJS/lajs/ch02/public_html/js/app.js
@@ -1,25 +1,33 @@
 'use strict';
 
-/* App Module */
+// This is the application module.
+// We bind this to <html> via ng-app.
+var helloWorldApp = angular.module(
+    'helloWorldApp', // The name of our app module.
+    // The other modules it depends on.
+    // ngRoute lets us do client-side routing of HTML templates/partials to ng-view via URL fragments.
+    // We've factored out our custom controllers into another module to be, well, modular.
+    ['ngRoute', 'helloWorldControllers']
+);
 
-var helloWorldApp = angular.module('helloWorldApp', [
-    'ngRoute',     
-    'helloWorldControllers'
-    
-]);
-
-
+// Service names start with $.  To inject them in a way that won't break if this code is minified, we use
+// the syntax that puts the service names in an array.
 helloWorldApp.config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
         $routeProvider.
                 when('/', {
+                    // Use the main partial view template for our base URL.
                     templateUrl: 'partials/main.html',
                     controller: 'MainCtrl'
                 }).when('/show', {
+                    // Route to the "show" partial view template for <base URL>/show.
+                    // Note that you can register a different controller to handle the new view.
                     templateUrl: 'partials/show.html',
                     controller: 'ShowCtrl'
                 });
 
+        // This forces us to use the #! syntax for URL fragments to route by.
+        // You can get more natural-looking URLs using the HTML 5 mode, but not all browsers support it.
         $locationProvider.html5Mode(false).hashPrefix('!');
     }]);
 
--- JavaScript/AngularJS/lajs/ch02/public_html/js/controllers.js
+++ JavaScript/AngularJS/lajs/ch02/public_html/js/controllers.js
@@ -1,17 +1,16 @@
 'use strict';
 
-/* Controllers */
+// This is a non-app module in which we define our custom controllers.
+// The only difference between the controllers is the message data stored in their scope.
 
 var helloWorldControllers = angular.module('helloWorldControllers', []);
 
-
-
 helloWorldControllers.controller('MainCtrl', ['$scope', '$location', '$http',
     function MainCtrl($scope, $location, $http) {
-        $scope.message = "Hello World";
+        $scope.message = "Hello, AngularJS! (MainCtrl)";
     }]);
 
 helloWorldControllers.controller('ShowCtrl', ['$scope', '$location', '$http',
     function ShowCtrl($scope, $location, $http) {
-        $scope.message = "Show The World";
+        $scope.message = "Hello, AngularJS (ShowCtrl)";
     }]);
