58e148c00e6a66094239be6864bdda708113d5d9
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-09 09:40:37 -0700

    Add more comments

--- JavaScript/AngularJS/lajs/ch04/public_html/js/app.js
+++ JavaScript/AngularJS/lajs/ch04/public_html/js/app.js
@@ -3,12 +3,16 @@
 (function() {
 
     var app = angular.module('helloWorld.app', [
-        'ngRoute',
-        'helloWorld.controllers'
+        'ngRoute', // Build in AngularJS module that does client-side URL routing.
+        'helloWorld.controllers' // Our custom controllers.
 
     ]);
 
-
+    // Route various URL requests directly back to our app.
+    // This prevents HTTP requests to the server.
+    // The routing associates a controller with a view/template/partial.
+    // This pair replaces the ng-view tagged DOM element in index.html.
+    // Parts of the URL can be parsed out into "route parameters" which the controller can access via $routeParams.
     app.config(['$routeProvider', '$locationProvider',
         function ($routeProvider, $locationProvider) {
             $routeProvider.when('/', {
@@ -24,10 +28,13 @@
                 templateUrl: 'partials/newCustomer.html',
                 controller: 'AddCustomerCtrl'
             }).when('/addedCustomer/:customer/:city', {
+                // The above makes it so we can get customer and city via $routeParams.
                 templateUrl: 'partials/addedCustomer.html',
                 controller: 'AddedCustomerCtrl'
             });
 
+            // Forces us to use !#/ prefix before locally routed URLs.
+            // I think this is only necessary for compatibility with older browsers.
             $locationProvider.html5Mode(false).hashPrefix('!');
         }]);
 
--- JavaScript/AngularJS/lajs/ch04/public_html/js/controllers.js
+++ JavaScript/AngularJS/lajs/ch04/public_html/js/controllers.js
@@ -31,13 +31,18 @@ controllers.controller('CustomerCtrl', ['$scope',
 
 controllers.controller('AddCustomerCtrl', ['$scope', '$location',
     function ($scope, $location) {
+        // This is bound to HTML form via ng-submit.
+        // cName and cCity are bound to HTML input elements.
         $scope.submit = function () {
+            // Move to a new URL.
+            // This does not cause an HTTP request since we're going to a URL routed to this SPA.
             $location.path('/addedCustomer/' + $scope.cName + "/" + $scope.cCity);
         };
     }]);
 
 controllers.controller('AddedCustomerCtrl', ['$scope', '$routeParams',
     function ($scope, $routeParams) {
+        // Parse the customer name and city out of the URL via $routeParams.
         $scope.customerName = $routeParams.customer;
         $scope.customerCity = $routeParams.city;
     }]);
