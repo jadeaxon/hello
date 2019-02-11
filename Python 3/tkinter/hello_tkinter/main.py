966e81a9f1337f505b7ef85d31ace1b70057aa73
Jeff Anderson <jadeaxon@hotmail.com>
2019-02-11 14:09:00 -0700

    Get Tkinter working

---
+++ Python 3/tkinter/hello_tkinter/main.py
@@ -0,0 +1,11 @@
+import tkinter
+
+# PRE: You've installed ActiveState ActiveTcl.
+
+# Print the Tkinter version.
+version = tkinter.Tcl().eval('info patchlevel')
+print(version)
+
+tkinter._test()
+
+
