e197789a00baa6a35b4fa1e071c26b4978db5fff
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-16 11:58:23 -0700

    Write FuploadTrailerRecord class

--- PL-SQL/fupload.sql
+++ PL-SQL/fupload.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE TYPE FuploadDocumentRecord AS OBJECT (
 */
 
 DROP TYPE FuploadHeaderRecord;
+DROP TYPE FuploadTrailerRecord;
 
 
 -- Abstract base class for the 4 types of atomic FUPLOAD records.
@@ -65,6 +66,38 @@ END;
 /
 
 
+-- FUPLOAD trailer record interface spec.  Inherits from the base record type.
+CREATE OR REPLACE TYPE FuploadTrailerRecord UNDER FuploadBaseRecord (
+	-- The transaction date in YYYYMMDD format.
+	rec_count char(8), -- Number of atomic records in the FUPLOAD finance document record.
+	trans_tot char(12), -- The total of the absolute value of each atomic detail record amount.
+
+	-- An atomic FUPLOAD record has a fixed length of 148 bytes.
+	-- This filler field aligns with that record boundary.
+	-- It should contain all space characters (ASCII 32).
+	filler char(111),
+	OVERRIDING MEMBER FUNCTION toString RETURN varchar2
+);
+/
+
+
+-- FUPLOAD trailer record implementation body.
+CREATE OR REPLACE TYPE BODY FuploadTrailerRecord AS
+	OVERRIDING MEMBER FUNCTION toString RETURN varchar2 IS
+		r varchar2(148) := '';
+	BEGIN
+		r := self.system_id;
+		r := r || self.doc_code;
+		r := r || self.rec_type;
+		r := r || self.rec_count;
+		r := r || self.trans_tot;
+		r := r || self.filler;
+		return r;
+	END toString;
+END;
+/
+
+
 -- Show specific compilation errors, if any.
 show errors;
 
@@ -72,12 +105,19 @@ show errors;
 -- Test using the FUPLOAD object types.
 DECLARE
 	hr1 FuploadHeaderRecord;
+	tr1 FuploadTrailerRecord;
 	r varchar2(148);
 
 BEGIN
 	dbms_output.put_line('Testing FUPLOAD object types.');
+
 	hr1 := FuploadHeaderRecord('DATALOAD', '', '1', '20190216', '');
 	r := hr1.toString();
+	dbms_output.put_line(REPLACE(r, ' ', '_'));
+	dbms_output.put_line(LENGTH(r));
+
+	tr1 := FuploadTrailerRecord('DATALOAD', '', '2', '20190216', '3', '123.45');
+	r := tr1.toString();
 	dbms_output.put_line(REPLACE(r, ' ', '_'));
 	dbms_output.put_line(LENGTH(r));
 
