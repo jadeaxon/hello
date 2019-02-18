5b31d58199519458d668d8db4210e0f8c4d8f2bb
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-17 19:32:34 -0700

    Connect to Oracle from Python 3 via cx_Oracle

---
+++ Python 3/oracle/hello_oracle/connect.py
@@ -0,0 +1,26 @@
+# https://cx-oracle.readthedocs.io/en/latest/index.html
+# Even though PyCharm thinks this module DNE, it does.
+# Conforms to Python Database API Specification 2.0.
+#
+# PRE: Make a python3 user in your Oracle database.
+# PRE: Install Oracle database on localhost.
+# PRE: Install Oracle client files.
+# PRE: Install cx_Oracle Python module.
+# PRE: Put user python3's password in password.dat file.
+# PRE: Create table python3.person table.  Insert some data.
+import cx_Oracle
+
+file = open("password.dat")
+password = file.read().strip()
+
+connection = cx_Oracle.connect('python3/{}@localhost'.format(password));
+
+cursor = connection.cursor();
+query = 'select * from person';
+cursor.execute(query);
+# This returns all the rows fetched by the query as tuples.
+rows = cursor.fetchall();
+for row in rows:
+    print(row)
+
+
