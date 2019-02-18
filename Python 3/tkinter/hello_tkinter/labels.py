feddc1d9c5c6aca49fc38c85f9e67b2305226917
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-18 10:19:27 -0700

    Demo Tkinter labels

---
+++ Python 3/tkinter/hello_tkinter/labels.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+label1 = ttk.Label(root, text="This is the first label.")
+label1.grid(column=1, row=1)
+
+# The label widget does not fully support animated gifs.
+# Using the 'compound' option, the image can be displayed below the text.
+image = PhotoImage(file="ascii_art.gif")
+label2 = ttk.Label(root, text="Display an image in a label.", compound='bottom')
+label2.grid(column=1, row=2) # grid() doesn't return the label widget, so we can't chain it above.
+label2['image'] = image
+
+root.mainloop()
