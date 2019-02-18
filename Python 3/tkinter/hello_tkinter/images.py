e4068a54f5cbe59ea85365038d6212fc1baa7b20
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-18 08:56:45 -0700

    Display text and image in the same label

--- Python 3/tkinter/hello_tkinter/images.py
+++ Python 3/tkinter/hello_tkinter/images.py
@@ -6,7 +6,8 @@ root = Tk()
 image = PhotoImage(file="ascii_art.gif")
 
 # The label widget does not fully support animated gifs.
-label = ttk.Label(root, text="Display an image.")
+# Using the 'compound' option, the image can be displayed below the text.
+label = ttk.Label(root, text="Display an image in a label.", compound='bottom')
 label.grid() # grid() doesn't return the label widget, so we can't chain it above.
 label['image'] = image
 
