8d4527a38d2f07b38567aed51a7bcb3e48a21ac3
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-24 16:08:58 -0700

    Define a custom attribute directive

--- JavaScript/AngularJS/lajsi1d/ch12/ch12.js
+++ JavaScript/AngularJS/lajsi1d/ch12/ch12.js
@@ -1,6 +1,17 @@
 var app = angular.module("app", []);
 
 app.controller("MainCtrl", function ($scope) {
-  $scope.message = "This is chapter 12."
+  $scope.chapter = 12;
+  $scope.topic = "Custom Directives"
+  $scope.message = "This is chapter " + $scope.chapter + ".";
 
+
+});
+
+// Define a custom attribute directive.
+// If we apply jsa-title to any element, it will replace it with the HTML template.
+app.directive("jsaTitle", function (){
+  return {
+    template: "<h2>Chapter {{chapter}}: {{topic}}</h2>"
+  };
 });
