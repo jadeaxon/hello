624b638b1dd4856bcb19bd57fa74eb0816096dfa
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-23 17:57:51 -0700

    Make a form to add to a list of people

--- JavaScript/AngularJS/lajsi1d/ch06/ch06.js
+++ JavaScript/AngularJS/lajsi1d/ch06/ch06.js
@@ -2,4 +2,20 @@ var app = angular.module("app", []);
 
 app.controller("MainCtrl", function ($scope) {
   $scope.message = "This is chapter 6."
+
+  $scope.person = {
+    firstName: null,
+    lastName: null
+  };
+
+  $scope.people = [];
+
+  $scope.addPerson = function () {
+    console.log('Pushed the Add Person button.');
+    // Clone the existing person object and push the clone onto our list of people.
+    // $scope.people.push(JSON.parse(JSON.stringify($scope.person)));
+    $scope.people.push($scope.person);
+    $scope.person = { firstName: null, lastName: null }; // Make a new person.
+    console.log($scope.people)
+  };
 });
