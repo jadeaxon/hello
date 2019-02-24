6a05362e7dc249fc098dffe48fca01a7b75f112e
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-24 16:22:07 -0700

    Add some comments

--- JavaScript/AngularJS/lajsi1d/ch12/ch12.js
+++ JavaScript/AngularJS/lajsi1d/ch12/ch12.js
@@ -10,8 +10,10 @@ app.controller("MainCtrl", function ($scope) {
 
 // Define a custom attribute directive.
 // If we apply jsa-title to any element, it will replace it with the HTML template.
+// jsaTitle => jsa-title in the HTML.
 app.directive("jsaTitle", function (){
   return {
+    // Note that the directive has access to its parent element's scope.
     template: "<h2 id='jsa-title'>Chapter {{chapter}}: {{topic}}</h2>"
   };
 });
