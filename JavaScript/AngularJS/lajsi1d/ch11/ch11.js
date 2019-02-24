ca04f5cd2e9e45a3f09552050cd5a7677f8f5c1d
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-24 15:57:13 -0700

    Use various built-in directives

--- JavaScript/AngularJS/lajsi1d/ch11/ch11.js
+++ JavaScript/AngularJS/lajsi1d/ch11/ch11.js
@@ -3,4 +3,16 @@ var app = angular.module("app", []);
 app.controller("MainCtrl", function ($scope) {
   $scope.message = "This is chapter 11."
 
+  $scope.holidays = [
+    'Thanksgiving',
+    'Easter',
+    'Christmas',
+    "Valentine's Day",
+    'Labor Day',
+    'Spring Break',
+    'Memorial Day',
+    'Independence Day',
+    'Pioneer Day'
+  ]
+
 });
