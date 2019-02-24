4fb1a06fe06dedb89b45206704cf3a6a4e4109ce
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-24 16:38:12 -0700

    Use a specific controller in the custom directive

--- JavaScript/AngularJS/lajsi1d/ch12/ch12.js
+++ JavaScript/AngularJS/lajsi1d/ch12/ch12.js
@@ -2,18 +2,23 @@ var app = angular.module("app", []);
 
 app.controller("MainCtrl", function ($scope) {
   $scope.chapter = 12;
-  $scope.topic = "Custom Directives"
+  $scope.topic = "Custom Directives";
   $scope.message = "This is chapter " + $scope.chapter + ".";
 
-
+  // A controller attribute.
+  this.ctrlid = 'jsa-title1';
 });
 
 // Define a custom attribute directive.
 // If we apply jsa-title to any element, it will replace it with the HTML template.
 // jsaTitle => jsa-title in the HTML.
-app.directive("jsaTitle", function (){
+app.directive("jsaTitle", function () {
   return {
     // Note that the directive has access to its parent element's scope.
-    template: "<h2 id='jsa-title'>Chapter {{chapter}}: {{topic}}</h2>"
+    // You can also access attributes of the controller, not just its scope.
+    controller: "MainCtrl",
+    controllerAs: "ctrl", // The this.* of the controller, not $scope.
+    // Note how you can {{}} interpolate inside an HTML attribute value.
+    template: "<h2 id='{{ctrl.ctrlid}}'>Chapter {{chapter}}: {{topic}}</h2>"
   };
 });
