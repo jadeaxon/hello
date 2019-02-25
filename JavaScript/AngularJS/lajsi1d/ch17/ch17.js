dc7f4436418578e67ef498ae04779174594d3ed0
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-25 12:06:06 -0700

    Make table of guitarists

--- JavaScript/AngularJS/lajsi1d/ch17/ch17.js
+++ JavaScript/AngularJS/lajsi1d/ch17/ch17.js
@@ -3,5 +3,43 @@ var app = angular.module("app", []);
 app.controller("MainCtrl", function ($scope) {
   $scope.message = "Chapter 17: Tables";
 
+  $scope.guitarists = [
+    {
+      name: "Eddie Van Halen",
+      song: "Eruption",
+      technique: "tapping"
+    },
+    {
+      name: "Eric Johnson",
+      song: "East Wes",
+      technique: "harp harmonics"
+    },
+    {
+      name: "Joe Satriani",
+      song: "Ice #9",
+      technique: "whammy bar lizard slide"
+    },
+    {
+      name: "Santana",
+      song: "Black Magic Woman",
+      technique: "supernatural slide bend"
+    },
+    {
+      name: "Jimi Hendrix",
+      song: "All Along the Watchtower",
+      technique: "voodoo unison bend"
+    },
+    {
+      name: "Yngwie Malmsteen",
+      song: "Black Star",
+      technique: "neoclassical sweep picking"
+    },
+    {
+      name: "Steve Vai",
+      song: "For the Love of God",
+      technique: "bee telepathy"
+    }
+  ];
+
 });
 
