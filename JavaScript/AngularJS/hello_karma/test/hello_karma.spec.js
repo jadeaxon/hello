6af4f9cd083c51e1559ec72cc177b00c5d865892
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-26 06:56:49 -0700

    Unit test our custom <jsa-title> directive

--- JavaScript/AngularJS/hello_karma/test/hello_karma.spec.js
+++ JavaScript/AngularJS/hello_karma/test/hello_karma.spec.js
@@ -33,3 +33,30 @@ describe('hello karma app', function() {
         });
     });
 });
+
+// Test a custom directive.
+describe('<jsa-title>', function () {
+  var $compile, $rootScope, $scope;
+
+  // This makes our controllers available.
+  beforeEach(module('karma.app'));
+
+  beforeEach(inject(function (_$compile_, _$rootScope_, _$controller_) {
+    $compile = _$compile_;
+    $scope = _$rootScope_.$new();
+    // Might be able to just do $scope = {} here and not use $rootScope at all.
+    // <jsa-title> uses $scope.message, so we need to construct a MainCtrl controller to initialize that.
+    $controller = _$controller_('MainCtrl', {$scope: $scope});
+  }));
+
+  it('<jsa-title> works in MainCtrl', function () {
+    // Compile a piece of HTML containing the directive in the context of a MainCtrl $scope.
+    var element = $compile("<jsa-title></jsa-title>")($scope);
+    // This updates all the data bindings.
+    // Apparently, when an element is compiled, it just sets up the watches/bindings but does not execute them.
+    // So, to actually get {{message}} to use $scope.message, we need to call $scope.$digest().
+    // If you comment out the line below, the test will fail.
+    $scope.$digest();
+    expect(element.html()).toContain("Hello, Karma!");
+  });
+});
