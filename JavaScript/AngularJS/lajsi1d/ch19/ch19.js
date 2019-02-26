6017bc0ebd0aa32150e0ad58414b2d1f8bd8c004
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-25 18:32:01 -0700

    Handle form submission

--- JavaScript/AngularJS/lajsi1d/ch19/ch19.js
+++ JavaScript/AngularJS/lajsi1d/ch19/ch19.js
@@ -2,5 +2,10 @@ var app = angular.module("app", []);
 
 app.controller("MainCtrl", function ($scope) {
   $scope.message = "Chapter 19: Form Submission";
+  $scope.submitted = false;
+
+  $scope.processForm = function () {
+    $scope.submitted = true;
+  };
 });
 
