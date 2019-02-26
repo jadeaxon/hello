f528d3ce88df5a1a053367331ecf560be17db872
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-26 06:40:21 -0700

    Add a custom element directive

--- JavaScript/AngularJS/hello_karma/site/js/app.js
+++ JavaScript/AngularJS/hello_karma/site/js/app.js
@@ -1,2 +1,9 @@
 // Edited externally in gVim.
 var app = angular.module('karma.app', ['karma.controllers']);
+
+app.directive("jsaTitle", function () {
+  return {
+    template: "<h2 class='jsa-title'>{{message}}</h2>",
+    restrict: "E"
+  };
+});
