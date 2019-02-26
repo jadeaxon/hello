a70f39c00b04f0fb4d47361b7992605cf3131971
Jeff Anderson <jeff.anderson@digecor.com>
2019-02-25 20:40:11 -0700

    Use ng-include to include files

--- JavaScript/AngularJS/lajsi1d/ch20/ch20.js
+++ JavaScript/AngularJS/lajsi1d/ch20/ch20.js
@@ -4,8 +4,16 @@ app.controller("MainCtrl", function ($scope) {
   $scope.message = "Chapter 20: ng-include";
   $scope.submitted = false;
 
+  $scope.includeFiles = true;
+
   $scope.processForm = function () {
     $scope.submitted = true;
   };
+});
+
+// Load external content into a div.
+$(function () {
+  // It places it inside the <div>; it does not replace the <div>.
+  $('div#content').load('content.html');
 });
 
