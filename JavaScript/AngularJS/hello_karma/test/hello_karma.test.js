63955eb7e6ceb432e58746e0e88ede6457aeb932
Jeff Anderson <jeff.anderson@digecor.com>
2019-03-01 14:25:18 -0700

    Get a Protractor test to work from WebStorm

--- JavaScript/AngularJS/hello_karma/test/hello_karma.test.js
+++ JavaScript/AngularJS/hello_karma/test/hello_karma.test.js
@@ -3,9 +3,9 @@
 // PRE: webdriver-manager update
 // PRE: webdriver-manager start
 // PRE: Create protractor.conf.js
-// protractor protractor.conf.js
+// PRE: Follow these instructions to make a Protractor run/debug config: https://www.jetbrains.com/help/webstorm/protractor.html.
+// PRE: Configure this in WebStorm: "Allow unsigned requests" option enabled (Settings | Build, Execution, Deployment | Debugger).
 
-// FAIL: The app doesn't load in the remote-controlled browser.
 describe('End-to-end app tests', function () {
   beforeEach(function () {
     browser.get('http://localhost:63342/hello_karma/site/index.html');
