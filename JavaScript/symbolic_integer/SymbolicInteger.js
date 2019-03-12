9b27448d74d309f82bc9e60d6dc0a607da972fc8
Jeff Anderson <jeff.anderson@digecor.com>
2019-03-12 15:43:30 -0600

    Let constructor use symbolic value

--- JavaScript/symbolic_integer/SymbolicInteger.js
+++ JavaScript/symbolic_integer/SymbolicInteger.js
@@ -3,9 +3,41 @@ function SymbolicInteger(value, template) {
   // A 2D array specifying what symbols are legal in each place of the symbolic integer.
   // Least significant place first.
   this.template = template;
-
-  this.value = value;
+  this.value = null;
   this.truncateZeros = false;
+
+  if (typeof value == "number") {
+    this.value = value;
+  }
+  else if (typeof value == "string") {
+    // Figure out the integer value based on the symbol string.
+    this.value = 0;
+    var multiplier = 1;
+    var symbols = value.split("").reverse();
+    for (var i = 0; i < symbols.length; i++) {
+      if (i > 0) {
+        multiplier = multiplier * this.template[i - 1].length;
+      }
+
+      var symbol = symbols[i];
+      var place_value = this.template[i].indexOf(symbol);
+      if (place_value == -1) {
+        console.log("ERROR: Symbol " + symbol + " not allowed at place " + i + ".");
+        // TO DO: Throw exception.
+      }
+
+      /// console.log("multiplier = " + multiplier);
+      /// console.log("symbol = " + symbol);
+      /// console.log("place = " + i);
+      /// console.log("place value = " + place_value);
+
+      this.value = this.value + (place_value * multiplier);
+    } // next symbol
+  }
+  else { // Illegal value type.
+    console.log("ERROR: Illegal value type for SymbolicInteger: " + typeof value);
+    // TO DO: Throw exception.
+  }
 
   this.toSymbols = function () {
     var remaining = this.value;
@@ -58,6 +90,10 @@ function SymbolicInteger(value, template) {
       console.log("ERROR: Value overflow: " + this.value);
     }
     return symbols;
+  }; // toSymbols();
+
+  this.getValue = function () {
+    return this.value;
   };
 
 } // SymbolicInteger()
@@ -76,6 +112,9 @@ function testSymbolicInteger() {
     console.log(i + ' => ' + s);
   }
 
+  var si2 = new SymbolicInteger("C-c", template1);
+  console.log("si2.getValue() = " + si2.getValue());
+
   // Count with lowercase alphanumeric symbols.
   var lcalphanum = '0123456789abcdefghijklmnopqrstuvwxyz'.split('');
   var template2 = [
@@ -83,6 +122,9 @@ function testSymbolicInteger() {
     lcalphanum,
     lcalphanum
   ];
+
+  var si3 = new SymbolicInteger("06w", template2); // 248
+  console.log("si3.getValue() = " + si3.getValue());
 
   for (var i = 0; i <= 256; i++) {
     var si = new SymbolicInteger(i, template2);
