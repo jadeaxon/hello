f5f129e6a779270e40781d51a18d5dabd60de5a9
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-09 21:10:03 -0600

    Show interpolation

--- PHP/variables.php
+++ PHP/variables.php
@@ -4,8 +4,11 @@
 // ""-quoted string literals.
 // The . operator for concatenation.
 <?php
+	$name = "Jeff";
 	$i = 3;
 	$j = 10;
 	echo "$i + $j = " . ($i + $j);
+	echo "\n"; // Newline.
+	echo "My name is ${name}." // Interpolation in a string literal.
 ?>
 
