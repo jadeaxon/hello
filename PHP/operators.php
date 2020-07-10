34aa12709c41eb7d9e76be512723360930e70c7d
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-10 13:32:42 -0600

    Calculate basic notes on a guitar

--- PHP/operators.php
+++ PHP/operators.php
@@ -1,5 +1,13 @@
 <?php
 	// Demo various PHP operators.
-	echo "left" . " " . "right"; // concatenation
+	echo "left" . " " . "right\n"; // concatenation
+
+	// Basic notes on a guitar.
+	$strings = 6;
+	$frets = 24;
+	$notes = $strings * $frets;
+	$notes = $notes + $strings; // Count the open strings.
+	echo "There are ${notes} basic notes on a ${strings}-string guitar with ${frets} frets.\n";
+
 ?>
 
