0ec3fa4a7464394cb4a101af3ccbf4b77b32ddf8
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-10 15:15:42 -0600

    Loop over an array

--- PHP/loops.php
+++ PHP/loops.php
@@ -2,9 +2,19 @@
 	echo "Hello, PHP loops!\n";
 
 	// Basic for loop.
+	echo "Count to 10:\n";
 	for ($i = 1; $i <= 10; $i++) {
 		echo "${i}\n";
 	}
+	echo "\n";
+
+	// Loop over an array.
+	$guitarists = ["Vai", "Satch", "EVH", "EJ", "Hendrix", "Santana"];
+	echo "Guitarists:\n";
+	for ($i = 0; $i < count($guitarists); $i++) {
+		echo $guitarists[$i] . "\n";
+	} // next guitarist
+	echo "\n";
 
 ?>
 
