ffa439f33057d5df4006ee6ed7a6f636e5b72049
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-10 14:40:27 -0600

    Begin arrays.php

---
+++ PHP/arrays.php
@@ -0,0 +1,32 @@
+<?php
+	echo "Hello, PHP arrays!\n";
+	$guitarists = array(); // Empty array.
+	$songs = []; // Also empty array.
+
+	$guitarists = array("Satriani", "EJ", "Santana", "Van Halen"); // Init array.
+
+	// Zero-based index.
+	echo $guitarists[0] . "\n";
+
+	// Append to end of array.
+	$guitarists[] = "Steve Vai";
+	array_push($guitarists, "Wes Montgomery", "Django Reinhardt", "Les Paul"); // Append multiple.
+
+	// FAIL: You cannot use -1 index to access last element.
+	// echo $guitarists[-1];
+
+	// Delete array element.
+	unset($guitarists[2]);
+
+	// Update element.
+	$guitarists[0] = "Satch";
+	echo $guitarists[0] . "\n";
+
+	// Split (explode) a string into array elements.
+	$values = [];
+	$input = "222,13,27,89";
+	$values = explode(",", $input);
+	echo $values[0] . "\n";
+
+?>
+
