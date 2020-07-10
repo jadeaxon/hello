2d7f413ee9ace2ae4f53f03d8285d5b42771ac9b
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-10 17:41:11 -0600

    Define a class and create an instance

--- PHP/classes.php
+++ PHP/classes.php
@@ -1,4 +1,24 @@
 <?php
 	echo "Hello, PHP classes!\n";
+
+	// A class for guitarists.
+	class Guitarist {
+		public $name;
+		public $pickSpeed;
+		public $tone;
+
+		// Constructor.
+		public function __construct() {
+			echo "Guitarist::__construct()\n";
+		}
+	}
+
+	// An instance of the guitarist class: Joe Satriani.
+	$satch = new Guitarist();
+	$satch->name = "Joe Satriani";
+	$satch->pickSpeed = 88;
+	$satch->tone = 80;
+
+
 ?>
 
