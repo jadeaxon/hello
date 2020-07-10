53133e487a0014cffad60b8af592a716f34528e6
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-10 13:37:04 -0600

    Begin conditionals.php

---
+++ PHP/conditionals.php
@@ -0,0 +1,20 @@
+<?php
+	// Demo various PHP operators.
+	echo "left" . " " . "right\n"; // concatenation
+
+	// Basic notes on a guitar.
+	$strings = 6;
+	$frets = 24;
+	$notes = $strings * $frets;
+	$notes = $notes + $strings; // Count the open strings.
+	echo "There are ${notes} basic notes on a ${strings}-string guitar with ${frets} frets.\n";
+
+	if ($notes >= 100) {
+		echo "That's a lot of notes!\n";
+	}
+	else {
+		echo "That's a reasonable amount of notes.\n";
+	}
+
+?>
+
