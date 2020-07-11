5b3915794f0e8da94ebb797ab5a7071e0eaa0437
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-10 20:48:31 -0600

    Create script to list a user in the database

---
+++ PHP/mysql/list_users.php
@@ -0,0 +1,26 @@
+<?php
+$host = "127.0.0.1";
+$username = "jadeaxon";
+$password = "flCsoXBFnugSTFs58A93";
+$database = "php";
+
+try {
+	$conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$statement = $conn->prepare("SELECT * FROM users WHERE email = :email");
+	$statement->execute(['email' => 'nobody@nowhere.tld']);
+	$statement->setFetchMode(PDO::FETCH_ASSOC);
+
+	// This fetches only the first matching user.
+	$user = $statement->fetch();
+
+	echo "<pre>";
+	print_r($user);
+	echo "</pre>";
+}
+catch(PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+}
+?>
+
