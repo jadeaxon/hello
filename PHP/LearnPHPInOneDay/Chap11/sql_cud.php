89d316e775a1d07350315e457264550db3368535
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-11 20:31:37 -0600

    Fix connection

--- PHP/LearnPHPInOneDay/Chap11/sql_cud.php
+++ PHP/LearnPHPInOneDay/Chap11/sql_cud.php
@@ -1,11 +1,11 @@
 <?php
 
-//SECTION A - CONNECT TO DATABASE
+// SECTION A - CONNECT TO DATABASE
 
-$pdo = new PDO("mysql:host=localhost;dbname=pawszone", "pz_admin", "ABCD");
+$pdo = new PDO("mysql:host=127.0.0.1;port=3307;dbname=pawszone", "pz_admin", "ABCD");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//SECTION B - CREATE TABLE
+// SECTION B - CREATE TABLE
 
 $sql = "CREATE TABLE IF NOT EXISTS pets ( owner VARCHAR(255) NOT NULL, petname VARCHAR(255) NOT NULL , breed VARCHAR(255) NOT NULL, microchip VARCHAR(20), PRIMARY KEY(owner, petname))";
 
@@ -13,7 +13,7 @@ $stmt = $pdo->prepare($sql);
 
 $stmt->execute();
 
-//SECTION C - INSERT DATA
+// SECTION C - INSERT DATA
 
 $sql = "INSERT INTO pets (owner, petname, breed)
 VALUES (:owner, :petname, :breed)";
@@ -24,16 +24,15 @@ $owner = array('Ted', 'Jamie', 'En', 'En');
 $pname = array('Angel', 'Max', 'Boots', 'Dora');
 $breed = array('Labradoodle', 'Domestic Shorthair', 'Domestic Shorthair', 'Munchkin');
 
-for ($i = 0; $i < 4; ++$i)
-{
+for ($i = 0; $i < 4; ++$i) {
     $stmt->bindValue(':owner', $owner[$i]);
     $stmt->bindValue(':petname', $pname[$i]);
     $stmt->bindValue(':breed', $breed[$i]);
-    
+
     $stmt->execute();
 }
 
-//SECTION D - UPDATE DATA
+// SECTION D - UPDATE DATA
 
 $sql = "UPDATE pets SET microchip = :micro WHERE owner = :owner AND petname = :petname";
 
@@ -45,7 +44,7 @@ $stmt->bindValue(':petname', 'Max');
 
 $stmt->execute();
 
-//SECTION E - DELETE DATA
+// SECTION E - DELETE DATA
 
 $sql = "DELETE FROM pets WHERE owner = :owner AND petname = :petname";
 
@@ -55,3 +54,4 @@ $stmt->bindValue(':owner', 'Ted');
 $stmt->bindValue(':petname', 'Angel');
 
 $stmt->execute();
+
