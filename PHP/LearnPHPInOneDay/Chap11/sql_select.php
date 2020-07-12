b4f64736b5c4584b2ceecca592ff79287105ec0f
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-11 20:33:25 -0600

    Fix connection

--- PHP/LearnPHPInOneDay/Chap11/sql_select.php
+++ PHP/LearnPHPInOneDay/Chap11/sql_select.php
@@ -1,26 +1,27 @@
 <?php
 
-//Section A - Connecting to the database
+// Section A - Connecting to the database
 
-$pdo = new PDO("mysql:host=localhost;dbname=pawszone", "pz_admin", "ABCD");
+$pdo = new PDO("mysql:host=127.0.0.1;port=3307;dbname=pawszone", "pz_admin", "ABCD");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//Section B - SELECT all rows from pets
+// Section B - SELECT all rows from pets
 
 $sql = "SELECT petname, owner FROM pets";
 $stmt = $pdo->prepare($sql);
 
 $stmt->execute();
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo '<BR>Owner = '.$row['owner'].'<BR>';
     echo 'Pet Name = '.$row['petname'].'<BR>';
 }
 
 $stmt->execute();
 
-while ($row = $stmt->fetch(PDO::FETCH_NUM)){
+while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
     echo '<BR>Owner = '.$row[1].'<BR>';
     echo 'Pet Name = '.$row[0].'<BR>';
 }
+
+
