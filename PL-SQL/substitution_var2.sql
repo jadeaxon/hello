0c8fbe7b68869e8c48a4de6fae82f068f98297e8
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-18 07:51:44 -0600

    Mention using &&

--- PL-SQL/substitution_var2.sql
+++ PL-SQL/substitution_var2.sql
@@ -1,6 +1,7 @@
 -- Use a substitution varible.
 -- Set a user variable to its value so user only has to input it once.
 -- How do we define it on the command line instead?
+-- Note that you could also just use &&x to ask user for value of x once.
 DEFINE x=&input_x;
 SELECT '&x' x1, 'Hello, ' || '&x' || '!' x2 FROM dual;
 exit
