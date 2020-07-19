a505c8ff039e9d2010b0685548106a82109dffa1
Jeff Anderson <jeff.anderson@digecor.com>
2020-07-19 13:49:40 -0600

    Demo app with single-file components

---
+++ JavaScript/Vue.js/app1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	
+	<!-- Use my custom component. -->
+	<concise-greeter />
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+// Import my custom component.
+import ConciseGreeter from './components/ConciseGreeter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld: HelloWorld,
+	// Register my custom component.
+	ConciseGreeter: ConciseGreeter
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ JavaScript/Vue.js/app1/src/components/ConciseGreeter.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+	<p>Hi, {{user}}!</p>
+</div>
+</template>
+
+<script>
+module.exports = {
+	data: function () {
+		return { user: 'default user' }
+	}
+}
+</script>
+
+<style scoped>
+p {
+	text-color: purple
+}
+</style>
+
+
