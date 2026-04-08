b86dae12bcee478fbb2d56d1cdf63fc74a5feeed
Jeff Anderson <jadeaxon@gmail.com>
2026-04-08 10:03:20 -0600

    Update print.py

--- Python 3/print.py
+++ Python 3/print.py
@@ -3,14 +3,40 @@
 # Unlike in Python 2, you need to use () with print.
 print("Put", "space", "between", "args.")
 print("Automatically append a newline.")
+print()
 print("Suppress newline with the named arg 'end'", end="")
 print(" as above.")
 
+# Default separator is a space. Override using sep arg.
+# Default line end is newline. Override with end arg.
+print("foo", "bar", "baz", sep="-", end="|")
+print()
+
+# Use formatted strings to use variable values inside them.
 name = "Jeff"
 age = 40
 # printf style format string with % operator to bind a tuple to formatter.
-print("%s is %d." % (name, age)) 
+print("%s is %d." % (name, age))
 # A Pythonic format string.
 print("{0} is {1}.".format(name, age))
+# An actual formatted string (and f-string).
+print(f"{name} is {age}.")
+
+print(
+"""
+Print
+a
+multiline
+string.
+""", end=""
+)
+
+message = "print me!"
+print(
+f"""
+Multiline formatted string:
+{message}
+""", end=""
+)
 
 
