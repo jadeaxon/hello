d190cc9445cc89cd29ca7fa3808ba9e1aee215bd
Jeff Anderson <jadeaxon@gmail.com>
2026-04-08 20:17:27 -0600

    Start list.py

---
+++ Python 3/list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+l = [] # empty list
+l = [1, 2, 3]
+
+# Loop over the elements of the list.
+for e in l: print(e)
+print()
+
+# Remove the last element of the list.
+print(l)
+print(len(l)) # Get length of list.
+del l[-1]
+print(l)
+print(len(l))
+print()
+
+
+
+
