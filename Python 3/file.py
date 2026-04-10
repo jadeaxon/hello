4ef5c9bc92fc6f91756ffbde0454391ef3cb2b01
Jeff Anderson <jadeaxon@gmail.com>
2026-04-10 08:46:06 -0600

    Update file.py

--- Python 3/file.py
+++ Python 3/file.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Open a file for reading.  Echo each line to stdout.
+# Default encoding is UTF-8.
 f = open('/etc/hosts', 'r')
 while True:
 	line = f.readline()
@@ -9,4 +10,17 @@ while True:
 	else:
 		break
 f.close()
+
+# Better to use 'with' when working with files.
+with open('/etc/hosts', 'r') as f:
+    contents = f.read() # Read entire contents of file into a string.
+print(len(contents))
+
+
+# The file object acts as an iterator on the lines in the file.
+lines = 0
+with open('/etc/hosts', 'r') as file:
+    for line in file:
+        lines += 1
+print(lines)
 
