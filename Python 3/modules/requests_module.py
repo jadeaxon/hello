b8df3ea8daf179da5d746e57466ae9ab234d6c01
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-18 12:32:38 -0600

    Try to use an open weather API

--- Python 3/modules/requests_module.py
+++ Python 3/modules/requests_module.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
+import json
 
 # Make an HTTP request and get a response.
 response = requests.get('https://automatetheboringstuff.com/files/rj.txt')
@@ -14,6 +15,31 @@ line = response.text.split('\n')[0]
 print(line)
 print()
 
-response = requests.get('https://automatetheboringstuff.com/files/DNE.txt')
-response.raise_for_status() # Should get an HTTP 404 error.
+try:
+    response = requests.get('https://automatetheboringstuff.com/files/DNE.txt')
+    response.raise_for_status() # Should get an HTTP 404 error.
+except Exception as e:
+    print(e)
+
+# Use an online weather API.
+# FAIL: Getting 401 unauthorized error even though API key is activated.
+# Looks like you have to additionally subscribe to a free-ish plan to make actual calls.
+api_key = open('api_key.txt').read().strip()
+print(api_key)
+
+city = 'San Francisco'
+state = 'CA'
+country = 'US'
+limit = 1
+base_url = 'http://api.openweathermap.org/geo/1.0/direct'
+query = f'?q={city},{state},{country}&limit={limit}&appid={api_key}'
+url = base_url + query
+print(url)
+response = requests.get(url)
+response.raise_for_status()
+
+# The response content is JSON. Convert it to a Python dictionary.
+data = json.loads(response.text)
+print(data)
+
 
