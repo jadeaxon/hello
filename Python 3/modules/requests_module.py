88a332f924f3a68917f8d609787086ab12afa43d
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-18 11:59:56 -0600

    Use raise_from_status() method

--- Python 3/modules/requests_module.py
+++ Python 3/modules/requests_module.py
@@ -2,11 +2,18 @@
 
 import requests
 
+# Make an HTTP request and get a response.
 response = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 print(type(response))
 print(response.status_code) # HTTP 200 status if all went well.
+response.raise_for_status() # Raise an exception if HTTP request failed.
+
+# Use content from the response.
 print(len(response.text)) # Content received from URL as a string is in response.txt.
 line = response.text.split('\n')[0]
 print(line)
+print()
 
+response = requests.get('https://automatetheboringstuff.com/files/DNE.txt')
+response.raise_for_status() # Should get an HTTP 404 error.
 
