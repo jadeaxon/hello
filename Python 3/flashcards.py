5f81380448fd33456616c086235dc2e1e97ceb5e
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-19 13:05:25 -0600

    Update flashcards

--- Python 3/flashcards.py
+++ Python 3/flashcards.py
@@ -3,22 +3,23 @@
 import random
 
 flashcards = {
-    r'\n': 'ASCII linefeed (LF); aka, newline',
+    r'\n': 'ASCII linefeed (LF=10); aka, newline',
     r'\t': 'ASCII horizontal tab (TAB=9)',
-    r'\a': 'ASCII bell (BEL)',
+    r'\a': 'ASCII bell (BEL=7)',
     r'\#': 'DNE: # does not need to be escaped',
     r'\\': 'backslash',
-    r'\'': 'single quote',
-    r'\"': 'double quote',
+    r'\'': 'single quote (when inside double quotes)',
+    r'\"': 'double quote (when inside single quotes)',
     r'\f': 'ASCII formfeed (FF=12)',
-    r'\b': 'ASCII backspace (BS)',
+    r'\b': 'ASCII backspace (BS=8)',
     r'\N{name}': 'Unicode character by name',
-    r'\r': 'ASCII carriage return (CR)',
-    r'\v': 'ASCII vertical tab (VT)',
+    r'\r': 'ASCII carriage return (CR=13)',
+    r'\v': 'ASCII vertical tab (VT=11)',
     r'\uxxxx': 'Unicode char with 16-bit hex value xxxx',
     r'\Uxxxxxxxx': 'Unicode char with 32-bit hex value xxxxxxxx',
-    r'\ooo': 'Character with octal value ooo',
-    r'\xhh': 'Character with hex value hh'
+    r'\ooo': 'Character with octal value ooo; can use 1-3 octal digits',
+    r'\0': 'Octal 0, the ASCII NUL character; not a string terminator in Python',
+    r'\xhh': 'Character (or byte when in bytes literal) with hex value hh'
 }
 
 keys = list(flashcards.keys())
