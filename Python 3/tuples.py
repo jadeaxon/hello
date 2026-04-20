d5c46477d044362e82a288cfd9e664903d201074
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-20 11:18:38 -0600

    Add more examples

--- Python 3/tuples.py
+++ Python 3/tuples.py
@@ -4,19 +4,34 @@
 # But, they may contain immutable refs to mutable objects like lists.
 
 t = ()
+print(type(t))
 t = (1,) # Must use trailing comma for 1-tuples.
 t = (1, 2)
-t = ("one", 2)
+t = ("one", 2) # A tuple can contain mixed types.
 t = 1, 2, 3 # Parenthesis are optional but more clear.
+t = 1, # one-element tuple
+
+nt = ((1, 2), (3, "four")) # You can have nested tuples.
 
 t = t + t # concat
+t += (3, 4)
 t = t * 2 # repeat
+t *= 2
 print(t)
 
 v = t[0]
 
 # You can use tuple slices (but can't assign to them since tuple's are immutable).
 t2 = t[:]
+
+# Tuples are immutable. So, none of these will work:
+# t.append(4)
+# t[0] = 1
+# del t[0]
+
+# An immutable object can still have its name deleted.
+t2 = ("delete", "me")
+del t2
 
 t = (1, 2, 3)
 x, y, z = t # Assignment unpacking
@@ -39,6 +54,14 @@ print(tuple(t2))
 # Tuple class has just two methods.
 i = t.index(3) # Location of value 3 in tuple.
 c = t.count(3) # Number of 3 values in tuple.
+
+length = len(t)
+bool(()) and print("unexpectedly true") # empty tuple is false
+
+# A tuple is a sequence and thus iterable.
+for e in t:
+    print(e)
+
 
 
 
