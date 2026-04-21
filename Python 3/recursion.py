0066274013d62cc2d310f7840fb88d2b85005e56
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-20 22:28:18 -0600

    Begin recursion.py

---
+++ Python 3/recursion.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# You can have a function call itself. This is recursion.
+def factorial(n):
+    # Base case.
+    if n == 0: return 1
+    if n == 1: return 1
+    # Recursive case.
+    return n * factorial(n - 1)
+
+r = factorial(5) # 5 * 4 * 3 * 2 * 1
+print(r)
+
+
+# Find the nth Fibonacci number.
+def fibonacci(n):
+    if n == 1: return 0
+    if n == 2: return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+for i in range(1, 10):
+    r = fibonacci(i)
+    print(r)
+
