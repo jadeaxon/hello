ef188c0fb59572f44ce3693a9aff2957a83ff216
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-21 09:40:50 -0600

    Use math module

---
+++ Python 3/modules/math_module.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import math
+
+"""
+Builtin math functions:
+abs()
+pow()
+min()
+max()
+"""
+
+r = math.sqrt(2)
+print(r)
+
+r = math.ceil(1.7)
+print(r)
+
+r = math.floor(1.7)
+print(r)
+
+print(math.pi)
+print(math.e)
+
+
