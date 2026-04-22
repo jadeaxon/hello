83c1c277f2b505ab304826001b601170e1f1351f
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-21 21:27:17 -0600

    Tweak swim club data processing

--- Python 3/head-first-python/swimclub.py
+++ Python 3/head-first-python/swimclub.py
@@ -4,6 +4,9 @@
 import glob
 import os
 import statistics
+from pathlib import Path
+
+SWIM_DATA_DIR = 'data' # swim data subdir
 
 def parse_filename(filename):
     L = filename.removesuffix(".txt").split('-')
@@ -40,23 +43,35 @@ def hundredths_to_time(h):
 def read_swim_data(file):
     with open(file) as f:
         lines = f.readlines() 
-    file = file.removeprefix('data\\')
+    file = file.removeprefix(f'{SWIM_DATA_DIR}\\')
     r = parse_filename(file)
-    print(r)
-    print(lines[0], end="")
+    (name, age, distance, stroke) = r
+    # print(r)
+    # print(lines[0], end="")
     times = lines[0].strip().split(',')
+    str_times = times.copy()
     times = list(map(time_to_hundredths, times))
-    print(times)
+    # print(times)
     average = sum(times) / len(times) # in hundredths
     average = statistics.mean(times) # same thing
     average = int(round(average, 0))
-    print(average)
-    t = hundredths_to_time(average)
-    print(t)
-    print()
+    # print(average)
+    str_average = hundredths_to_time(average)
+    # print(t)
+    # print()
+    return (name, age, distance, stroke, str_times, str_average)
 
-print(os.getcwd())
-files = glob.glob('data/*.txt')
-for file in files:
-    read_swim_data(file)
+def main():
+    print(os.getcwd())
+    files = os.listdir(SWIM_DATA_DIR) # almost the same thing as below
+    files = glob.glob(f'{SWIM_DATA_DIR}/*.txt')
+
+    for file in files:
+        path = Path(file) # to get the file basename
+        print(f"Processing {path.name}...")
+        swim_data = read_swim_data(file)
+        print(swim_data)
+
+if __name__ == '__main__':
+    main()
     
