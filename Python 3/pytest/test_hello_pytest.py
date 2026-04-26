2edc32fa26dab11d37577b77b8dcf5f7274f1e83
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-26 10:28:09 -0600

    Begin pytest

---
+++ Python 3/pytest/test_hello_pytest.py
@@ -0,0 +1,11 @@
+# Any function that begins with test_ is seen as a test.
+# Also, the tests have to be inside a file that starts with test_.
+# You can use assert inside your tests.
+def test_pass():
+    assert 1 == 1
+
+def test_fail():
+    assert 1 != 1
+
+
+
