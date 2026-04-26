694e04a7828aeae5e44bf6c3f19b56cd97c877e4
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-26 10:35:44 -0600

    Add a couple more test files

---
+++ Python 3/pytest/test_failure.py
@@ -0,0 +1,11 @@
+def test_in_list():
+    assert 1 in [2, 3, 4]
+
+def test_not_in_str():
+    assert "fizz" not in "fizzbuzz"
+
+def test_less_than():
+    a = 10
+    b = 5
+    assert a < b
+
---
+++ Python 3/pytest/test_success.py
@@ -0,0 +1,12 @@
+def test_is():
+    assert True is True
+
+def test_is_not():
+    assert True is not False
+
+def test_add():
+    x = 2
+    y = 3
+    assert x + y == 5
+
+
