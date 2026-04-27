828b5b7661aa9cc20ca524166f642d8b75eb31e6
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-27 13:49:33 -0600

    Update pytest stuff

--- Python 3/pytest/ptwp/exercises/ch6/test_markers.py
+++ Python 3/pytest/ptwp/exercises/ch6/test_markers.py
@@ -1,26 +1,29 @@
 import pytest
 
+# Give all test in this file this mark.
+pytestmark = pytest.mark.all
 
+@pytest.mark.odd
 def test_one():
     pass
-
 
 def test_two():
     pass
 
-
+@pytest.mark.odd
 def test_three():
     pass
 
-
+@pytest.mark.testclass
 class TestClass:
     def test_four(self):
         pass
 
+    @pytest.mark.odd
     def test_five(self):
         pass
 
-
-@pytest.mark.parametrize("x", [6, 7])
+@pytest.mark.parametrize("x", [6, pytest.param(7, marks=pytest.mark.odd)])
 def test_param(x):
     pass
+
