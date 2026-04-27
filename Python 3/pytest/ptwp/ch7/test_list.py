565e9019dc0b6b7e9429b55f32d4ddf6c1ae88d0
Jeff Anderson <jadeaxon@gmail.com>
2026-04-27 16:21:08 -0600

    Finish chapter 9 exercises

--- Python 3/pytest/ptwp/ch7/test_list.py
+++ Python 3/pytest/ptwp/ch7/test_list.py
@@ -4,7 +4,8 @@ Test Cases
 * `list` from a non-empty database
 """
 from cards import Card
-
+from cards import MissingSummary
+import pytest
 
 def test_list_no_cards(cards_db):
     """Empty db, empty list"""
@@ -29,3 +30,37 @@ def test_list_several_cards(cards_db):
     assert len(the_list) == len(orig)
     for c in orig:
         assert c in the_list
+
+
+def test_list_card_without_summary(cards_db):
+    card = Card("", owner="me", state="todo")
+    with pytest.raises(MissingSummary) as e_info:
+        cards_db.add_card(card)
+
+
+def test_list_filter_by_owner(cards_db):
+    card = Card("test", owner="me", state="todo")
+    card2 = Card("test", owner="thee", state="todo")
+    cards_db.add_card(card)
+    cards_db.add_card(card2)
+    cards = cards_db.list_cards(owner="me")
+    assert cards[0] == card
+
+
+def test_list_filter_by_state(cards_db):
+    card = Card("test", owner="me", state="todo")
+    card2 = Card("test", owner="me", state="done")
+    cards_db.add_card(card)
+    cards_db.add_card(card2)
+    cards = cards_db.list_cards(state="todo")
+    assert cards[0] == card
+
+
+def test_list_filter_by_owner_and_state(cards_db):
+    card = Card("test", owner="me", state="todo")
+    card2 = Card("test", owner="thee", state="done")
+    cards_db.add_card(card)
+    cards_db.add_card(card2)
+    cards = cards_db.list_cards(owner="me", state="todo")
+    assert cards[0] == card
+
--- Python 3/pytest/ptwp/ch9/some_code/test_some_code.py
+++ Python 3/pytest/ptwp/ch9/some_code/test_some_code.py
@@ -1,10 +1,12 @@
 import foo_module
 import bar_module
 
-
 def test_foo():
     assert foo_module.foo() == "foo"
 
-
 def test_bar():
     assert bar_module.bar() == "bar"
+
+def test_baz():
+    assert bar_module.baz() == "baz"
+
