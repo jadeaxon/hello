7378e52842d0c3644d284f4a457f57876815794e
Jeff Anderson <jadeaxon@gmail.com>
2026-04-27 19:41:18 -0600

    Do chapter 10 exercises

--- Python 3/pytest/ptwp/exercises/ch10/test_my_info.py
+++ Python 3/pytest/ptwp/exercises/ch10/test_my_info.py
@@ -1,12 +1,14 @@
 from unittest import mock
 import my_info
-
+from pathlib import Path
 
 def test_my_home_returns_correct_value():
-    value = my_info.home_dir()
-    assert value == "/users/fake_user"
-
+    with mock.patch('pathlib.Path.home') as mock_home:
+        mock_home.return_value = Path("/users/fake_user")
+        value = my_info.home_dir()
+        assert value == "/users/fake_user"
 
 def test_my_home_is_called():
-    my_info.home_dir()
-    # check to see if Path.home() was called
+    with mock.patch('pathlib.Path.home') as mock_home:
+        my_info.home_dir()
+        mock_home.assert_called_with()
